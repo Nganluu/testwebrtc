56693126b4e5ea59873ed1e8eadb71875c35614a
nganltt <nganltt@vps.com.vn>
2022-04-29 16:32:36 +0700

    add sentry

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import reportWebVitals from './reportWebVitals';
 
+Sentry.init({
+  dsn: "https://4390561951674a97979b4e70a828aa88@o1220188.ingest.sentry.io/6370123",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
